9671829da6e673a6f1d7492dd95463a1b509e4ac
Александр Власов <velesov7493@yandex.ru>
2021-09-02 15:29:23 +0300

    2. Filter [#2517]

---
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -0,0 +1,30 @@
+package ru.job4j.dreamjob.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest servletRequest,
+            ServletResponse servletResponse,
+            FilterChain filterChain
+    ) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do") || uri.endsWith("index.do")) {
+            filterChain.doFilter(req, resp);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/auth.do");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
